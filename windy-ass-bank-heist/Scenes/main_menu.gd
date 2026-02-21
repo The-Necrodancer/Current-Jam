a899c290c9bf16811e300d0862f607525d165d60
Owen Benji K <113569166+The-Necrodancer@users.noreply.github.com>
2026-02-21 04:32:39 -0500

    Made a basic title screen
    Made a basic title screen lol.

---
+++ windy-ass-bank-heist/Scenes/main_menu.gd
@@ -0,0 +1,23 @@
+extends Control
+
+# Called when the node enters the scene tree for the first time.
+# func _ready():
+#	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+# func _process(delta):
+#	pass
+
+# Move to the credits page level
+func _on_credits_button_pressed():
+	print("Credits Button has been pressed!")
+	get_tree().change_scene_to_file('res://Scenes/credits.tscn')
+
+# Move to the first level
+func _on_play_button_pressed():
+	print("Play Button has been pressed!")
+
+func _on_quit_button_pressed():
+	print("Quit Button has been pressed!")
+	get_tree().quit()
