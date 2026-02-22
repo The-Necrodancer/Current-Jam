49136ef486c1bf678a3598fedd691161bdc8381c
Tealsquid <92330632+FilipSigda@users.noreply.github.com>
2026-02-21 20:58:18 -0500

    quick patch

---
+++ windy-ass-bank-heist/Camera.gd
@@ -0,0 +1,14 @@
+extends Camera2D
+class_name CameraFollow
+
+@export var target : Node2D
+
+@export var x_bounds : Vector2 = Vector2(0,0)
+
+func _ready() -> void:
+	if x_bounds.x - x_bounds.y < 1:
+		x_bounds = self.global_position.x * Vector2.ONE
+
+func _process(delta: float) -> void:
+	if target != null:
+		self.global_position.x = target.global_position.x
